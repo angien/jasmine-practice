a2559e34b09d90ea1dbcd910945b390c3845f80f
Angie Nguyen <anginguy@cisco.com>
2014-07-23 15:26:45 -0700

    first commit

---
+++ practice.js
@@ -0,0 +1,15 @@
+exports.add = function(x, y) {
+	return x+y;
+};
+
+exports.multiply = function(x, y) {
+	return x*y;
+};
+
+exports.or = function(x, y) {
+	return x || y;
+};
+
+exports.nothing = function() {
+
+};
---
+++ spec/practice-spec.js
@@ -0,0 +1,26 @@
+var practice = require("../practice");
+
+// like a test suite
+describe("test suite", function () {
+  // one unit test
+  it ("should add 2 and 3", function () {
+    var product = practice.add(2,3);
+    expect(product).toBe(5);
+  })
+
+  it ("should multiply 2 and 3", function () {
+    var product = practice.multiply(2,3);
+    expect(product).toBe(6);
+  })
+
+it ("should true OR false = true", function () {
+    var product = practice.or(true,false);
+    expect(product).toBe(true);
+  })
+
+it ("should return nothing", function () {
+    var product = practice.nothing();
+     expect(product).not.toBeDefined();
+  })
+
+})
